5bdc711cf97a0754cb56c554f2ca1860c0a90cda
Vyacheslav Osepyan <mr-93@mail.ru>
2020-06-06 22:21:50 +0300

    Updates

---
+++ precache-manifest.02843e6a98044720716e693d968e878f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc77bcdf224c0c1e419563ee00df108",
+    "url": "/healthApp/index.html"
+  },
+  {
+    "revision": "4bb2a83d43ef265abadc",
+    "url": "/healthApp/static/css/main.f9a96e88.chunk.css"
+  },
+  {
+    "revision": "a1218ca630f44c612937",
+    "url": "/healthApp/static/js/2.974c2f53.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/healthApp/static/js/2.974c2f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb2a83d43ef265abadc",
+    "url": "/healthApp/static/js/main.9860ec2e.chunk.js"
+  },
+  {
+    "revision": "bea6350e1375561837fc",
+    "url": "/healthApp/static/js/runtime-main.be219943.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f41d573e5f1c2482bca2d33b8e7586b9.js"
+  "/healthApp/precache-manifest.02843e6a98044720716e693d968e878f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/healthApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
